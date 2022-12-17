0e3f992b087f714b041242e7106cdcc202a0509d
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-17 18:59:22 +0300

    add cupboad

---
+++ Cupboad.java
@@ -0,0 +1,12 @@
+public class Cupboad {
+    private String condition;
+
+    public Cupboad(String condition) {
+        this.condition = condition;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+}
