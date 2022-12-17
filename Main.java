604b0030da1536c8745f1404f5f099c109dfa269
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-17 18:59:59 +0300

    add main

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Human andrey = new Human("Андрей", false);
+        Cupboad cupboadWithDishes = new Cupboad("open");
+        andrey.openCupboad(cupboadWithDishes);
+    }
+}
